0311c7748f4bc51ec90ca7e807dcf11b38e8e09f
Ifedayo Ogundana <ogundanaifedayo@gmail.com>
2026-04-06 10:14:33 -0500

    Update service worker cache version to v2

--- newshub/sw.js
+++ newshub/sw.js
@@ -1,5 +1,5 @@
 // NewsHub Service Worker v1
-const CACHE = 'newshub-v1';
+const CACHE = 'newshub-v2';
 const SHELL = ['/newshub/','/newshub/index.html','/newshub/manifest.json'];
 
 self.addEventListener('install', e => {
